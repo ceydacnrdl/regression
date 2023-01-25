dac8da2d64766723b4611170399a75636a400caa
Ceyda Çınardal <75117245+ceydacnrdl@users.noreply.github.com>
2023-01-25 20:16:19 +0300

    Update regression2.py

--- regression2.py
+++ regression2.py
@@ -1,24 +1,6 @@
 import numpy as np
 from sklearn.metrics import r2_score
 
-# a=np.array([[3,5,-9],[-8,-6,5],[10,3,-6]])
-# b= np.array([[-3],[10],[-8]])
-#
-# c=np.dot(np.linalg.inv(a),b)
-# print('Coefficients:')
-# print(c)
-#
-# c2=np.linalg.lstsq(a,b,rcond=None)[0]
-# print('Coefficients of Least Square method:')
-# print(c2)
-#
-# print('-'*50)
-# print('Predict:')
-# print(np.dot(a,c))
-# print('Predict of Least Square method:')
-# print(np.dot(a,c2))
-
-# x=np.array([[1.0,2.7,3.2,4.8,5.6]])
 x=np.array([[1,3,5,6,9,12,15]])
 x=x.T
 print(x.shape)
